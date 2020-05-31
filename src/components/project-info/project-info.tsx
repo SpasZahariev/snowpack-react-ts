9fdf0bdf3dd90fc0292065231c341a8ed918753b
Spas_Z <spaszahariev54@gmail.com>
2020-06-01 00:13:50 +0100

    initial commit

---
+++ src/components/project-info/project-info.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import "./project-info.css"
+
+
+const ProjectInfo = () => (
+
+    <div>
+        <img src="https://picsum.photos/500/800"></img>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing </p>
+    </div>
+)
+
+// this guide looks like what I want
+
+export default ProjectInfo;
---
+++ src/pages/app/App.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react'
+import ProjectInfo from '../../components/project-info/project-info';
+
+const App: React.FunctionComponent = () => {
+  return (
+
+    <div>
+      {navigationMenu()}
+      {getIntroduction()}
+      {getBackgroundParagraph()}
+      {getExperienceParagraph()}
+      {getSkillsTable()}
+      {getProjects()}
+      {getContactInfo()}
+    </div>
+
+  );
+}
+
+const navigationMenu = () => {
+  return <div></div>
+}
+
+
+const getIntroduction = () => {
+  return <section>
+    <h4>Hello, there - I'm</h4>
+    <h1>SPAS ZAHARIEV</h1>
+    <h1>I like creating things and solving problems</h1>
+    <h4>I'm a backend software engineer based in the UK, focusing on scalable and fault tolerant solutions</h4>
+    <button>Reach out pdf</button>
+    <button>Resume</button>
+  </section>
+}
+
+const getBackgroundParagraph = () => {
+  return <section>
+    <h1>About Me</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore unde consequuntur ex voluptates ad? Dolore, sapiente ea cum consequuntur eaque adipisci tempora nostrum quaerat ad sequi! Nostrum consectetur maiores libero?
+    </p>
+  </section>
+}
+
+const getExperienceParagraph = () => {
+  return <section>
+    <h1>experience timeline</h1>
+  </section>
+}
+
+const getSkillsTable = () => {
+  return <section>
+    <h1>Skills</h1>
+    <h1>Look at some skills</h1>
+  </section>
+}
+
+const getProjects = () => {
+  return <section>
+    <h1>Featured Projects</h1>
+    <div>
+      <ProjectInfo></ProjectInfo>
+      <ProjectInfo></ProjectInfo>
+      <ProjectInfo></ProjectInfo>
+    </div>
+  </section>
+}
+
+const getContactInfo = () => {
+  return <section>
+    <h1>Contact Info</h1>
+  </section>
+}
+export default App;
