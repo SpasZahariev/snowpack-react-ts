e71eb89f7155d0fca61ab6c533b98f72581cb486
Spas_Z <spaszahariev54@gmail.com>
2020-06-01 23:19:51 +0100

    fadin working

--- src/components/common/FadeInSection/fadeInSection.tsx
+++ src/components/common/FadeInSection/fadeInSection.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react'
-
+import "./fadeInSection.css"
 
 const FadeInSection: React.FC = (props) => {
-    const [isVisible, setVisible] = React.useState(true);
+    const [isVisible, setVisible] = React.useState(false);
     const domRef: React.MutableRefObject<any> = React.useRef();
+
+    //observer entries are just the div we have below
+    //the entries.forEach calback is called only when the div's "status" changes and it statrts intersecting
+    //unobserve function happens on unmount
     React.useEffect(() => {
         const observer = new IntersectionObserver(entries => {
             entries.forEach(entry => setVisible(entry.isIntersecting));
@@ -16,6 +20,7 @@ const FadeInSection: React.FC = (props) => {
             className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
             ref={domRef}
         >
+            {/* //this are all the child HTML components */}
             {props.children}
         </div>
     );
--- src/components/project-info/project-info.tsx
+++ src/components/project-info/project-info.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react'
 import "./project-info.css"
+import FadeInSection from '../common/FadeInSection/fadeInSection';
 
 
 const ProjectInfo = () => (
 
-    <div>
-        <img src="https://picsum.photos/500/800"></img>
+    <FadeInSection>
+        <div>
+            <img src="https://picsum.photos/500/800"></img>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing </p>
-    </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing </p>
+        </div>
+    </FadeInSection>
 )
 
 // this guide looks like what I want
